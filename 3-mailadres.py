08ad1bf8acf326e42bcb0b256b1f386988d5f4d0
sven1719 <212419@vistacollege.nl>
2022-05-19 10:07:20 +0200

    spelling

--- 3-mailadres.py
+++ 3-mailadres.py
@@ -6,4 +6,4 @@ patroonemail = r"[a-zA-Z0-9+]+@[a-zA-Z]+.[a-zA-Z]{2,}"
 if re.fullmatch(patroonemail, mail): 
     print("Dit is een correcte email")
 else:
-    print("Deze email is oncorrect")
+    print("Deze email is incorrect")
