c535c1daed675b177262d5ccd03a0cea4f2bf5d3
sven1719 <212419@vistacollege.nl>
2022-05-19 14:06:44 +0200

    name generator done

--- 18-NamenGenerator.py
+++ 18-NamenGenerator.py
@@ -1,9 +1,6 @@
 import names
 
-a = 0 
 while True:
-
-    input("druk op enter voor een andere naam.")
-    for i in range(1):
-        print(names.get_full_name(i))
-        a +=1
+    if input("Druk op enter voor een andere naam. (q to quit)") == "q":
+        break
+    print(names.get_full_name())
