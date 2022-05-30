df772e24469dcb6c40eaf3f8f9a9503822306a4a
sven1719 <212419@vistacollege.nl>
2022-05-30 10:42:00 +0200

    14 netheid

--- 14-CNaarFEnK.py
+++ 14-CNaarFEnK.py
@@ -1,5 +1,7 @@
 c = float(input("Type hier de graden in celsius:"))
+
 fahrenheit = c * 1.8 + 32
 kelvin = c + 273.15
+
 print(f"""Dit is je temparatuur in Fahrenheit:{fahrenheit}
 Dit is je temparatuur in Kelvin:{kelvin}""")
